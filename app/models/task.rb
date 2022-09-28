68fdf71f69bb792f98d479c539c3177b2cc4a8ce
Hironobu-okubo <ee171068oh@gmail.com>
2022-09-29 03:49:59 +0900

    loginuser method

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,8 @@ class Task < ApplicationRecord
   validates :name, length: {maximum: 30}
   validate :validate_name_not_including_comma
   
+  belongs_to :user
+  
   private
   
   def set_nameless_name
@@ -12,6 +14,6 @@ class Task < ApplicationRecord
   end
   
   def validate_name_not_including_comma
-    errors.add(:name, 'にカンマを含めることはできません') if name&.include(',')
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220928112433) do
+ActiveRecord::Schema.define(version: 20220928181112) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" do |t|
     t.string   "name"
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",                   null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" do |t|
